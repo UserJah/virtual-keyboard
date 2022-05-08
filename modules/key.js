30cd56a88ca5da76865888b2253cb858bb34d7d3
Rustam <dzhaksimov.rustam@gmail.com>
2022-05-08 17:23:22 +0300

    initial commit

--- modules/key.js
+++
@@ -1,7 +0,0 @@
-export default class Key {
-  constructor() {
-    this.node = document.createElement('button');
-    this.node.classList.add('key');
-    this.node.setAttribute('type', 'button');
-  }
-}
